bc74ba2d4310f15adad010913aa1cc71df2e4e80
Mayday4671 <405261806@qq.com>
2019-01-14 11:54:15 +0800

    6666

---
+++ src/main/java/com/zb/cs/CsApplication.java
@@ -0,0 +1,16 @@
+package com.zb.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsApplication.class, args);
+    }
+
+}
+
